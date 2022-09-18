53b0b9e5ae4b0a7dd77ac57bac78df8edb492872
LayneChan <3263252737@qq.com>
2022-09-18 13:05:56 +0800

    Spring配置类

---
+++ assignments/Week2/src/main/java/edu/whu/config/Config.java
@@ -0,0 +1,14 @@
+package edu.whu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author myAdministrator
+ * Spring配置类
+ */
+@Configuration()
+@ComponentScan(basePackages = "edu.whu")
+public class Config {
+
+}
