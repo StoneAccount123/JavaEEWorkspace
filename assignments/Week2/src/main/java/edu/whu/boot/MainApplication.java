8227bf4760b964529add7e18a83a9a645da6eddd
LayneChan <3263252737@qq.com>
2022-09-18 13:10:15 +0800

    有主要功能的主程序

---
+++ assignments/Week2/src/main/java/edu/whu/boot/MainApplication.java
@@ -0,0 +1,50 @@
+package edu.whu.boot;
+
+import edu.whu.annotation.InitMethod;
+import edu.whu.config.Config;
+import edu.whu.config.FileReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author myAdministrator
+ */
+public class MainApplication {
+    
+    public Class getMyClass(){
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        FileReader bean = context.getBean(FileReader.class);
+        try {
+             return Class.forName(bean.getBootstrapClass());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Object creatObj(){
+        try {
+            return getMyClass().newInstance();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public void invokeMethod(){
+        Object instance = creatObj();
+        Method[] methods = getMyClass().getDeclaredMethods();
+        for (Method method : methods) {
+            if(method.isAnnotationPresent(InitMethod.class)){
+                try {
+                    method.invoke(instance);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
