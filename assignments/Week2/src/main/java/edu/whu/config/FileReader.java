58a6d953a8534b30dd257918a9a6a5d7b4015b85
LayneChan <3263252737@qq.com>
2022-09-18 13:06:20 +0800

    读取配置文件

---
+++ assignments/Week2/src/main/java/edu/whu/config/FileReader.java
@@ -0,0 +1,22 @@
+package edu.whu.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author myAdministrator
+ * 读取配置文件的类
+ */
+@Component
+@Data
+@PropertySource({"classpath:myapp.properties"})
+public class FileReader {
+    /**
+     *
+     */
+    @Value("${bootstrapClass}")
+    private String bootstrapClass;
+
+}
