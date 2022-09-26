9e456958cd1db36311b6ce68e26a6909243f24f2
LayneChan <3263252737@qq.com>
2022-09-26 19:48:10 +0800

    自定义的注解

---
+++ assignments/Week3/src/main/java/edu/whu/context/annotation/Component.java
@@ -0,0 +1,17 @@
+package edu.whu.context.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author myAdministrator
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Component {
+
+    String value();
+}
